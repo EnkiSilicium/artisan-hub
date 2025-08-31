1d5e842d76caadcf1d982943b7dd5392af85eecb
Anton Popov <technowork17@gmail.com>
2025-08-31 23:15:51 +0400

    feat(bonus-service): add postgres-view based read projection.
    - Added a materialized view with basic queries.

---
+++ apps/bonus-service/src/app/modules/read-projection/application/bonus-read/bonus-read.query-handler.ts
@@ -0,0 +1,25 @@
+// read-model/vip-additive.read.service.ts
+import { Injectable } from '@nestjs/common';
+import { toBonusReadresultDto } from 'apps/bonus-service/src/app/modules/read-projection/application/bonus-read/bonus-read.mapper';
+import { BonusReadRepo } from 'apps/bonus-service/src/app/modules/read-projection/infra/persistence/repositories/bonus-read.repository';
+import { BonusReadQuery } from './bonus-read.query';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class BonusReadHandler {
+  private readonly repo: BonusReadRepo;
+
+  constructor(ds: DataSource) {
+    this.repo = new BonusReadRepo(ds);
+  }
+
+  async read(query: BonusReadQuery) {
+    const { total, rows } = await this.repo.read(query);
+    return toBonusReadresultDto(total, rows);
+  }
+
+  async refresh() {
+    await this.repo.refresh();
+    return { ok: true };
+  }
+}
