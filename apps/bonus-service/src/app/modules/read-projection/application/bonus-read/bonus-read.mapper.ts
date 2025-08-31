d0738eb38338c62deab72ee2fc5e5d9bdac782ef
Anton Popov <technowork17@gmail.com>
2025-08-31 23:21:03 +0400

    feat(bonus-service): add postgres-view based read projection. (#6)
    - Added a materialized view with basic queries.

---
+++ apps/bonus-service/src/app/modules/read-projection/application/bonus-read/bonus-read.mapper.ts
@@ -0,0 +1,34 @@
+// read-model/mappers/vip-additive.mapper.ts
+
+import { BonusReadProjection } from 'apps/bonus-service/src/app/modules/read-projection/infra/persistence/projections/bonus-read.projection';
+import { BonusReadFlatDto, BonusReadresultDto } from 'contracts';
+
+export const toBonusReadFlatDto = (
+  row: BonusReadProjection,
+): BonusReadFlatDto => ({
+  commissionerId: row.commissionerId,
+
+  isVIP: row.isVIP,
+  vipLastTickAt: row.vipLastTickAt,
+  vipLastBucket: row.vipLastBucket,
+  vipPolicyVersion: row.vipPolicyVersion,
+  windowAlgoPolicyVersion: row.windowAlgoPolicyVersion,
+  vipBonusPolicyVersion: row.vipBonusPolicyVersion,
+  vipCreatedAt: row.vipCreatedAt,
+  vipLastUpdatedAt: row.vipLastUpdatedAt,
+
+  totalPoints: row.totalPoints,
+  grade: row.grade,
+  bonusPolicyVersion: row.bonusPolicyVersion,
+  gradePolicyVersion: row.gradePolicyVersion,
+  bonusCreatedAt: row.bonusCreatedAt,
+  bonusLastUpdatedAt: row.bonusLastUpdatedAt,
+});
+
+export const toBonusReadresultDto = (
+  total: number,
+  rows: BonusReadProjection[],
+): BonusReadresultDto => ({
+  total,
+  items: rows.map(toBonusReadFlatDto),
+});
