f68fc9c832a005b6ed49ec3bfb3770828e46a848
Anton Popov <technowork17@gmail.com>
2025-09-06 08:03:13 +0400

    chore(shared-kernel): change error type

--- libs/shared-kernel/src/lib/domain/assertions/assert-is-object.assertion.ts
+++ libs/shared-kernel/src/lib/domain/assertions/assert-is-object.assertion.ts
@@ -1,7 +1,11 @@
+import { ProgrammerError } from "error-handling/error-core";
+import { ProgrammerErrorRegistry } from "error-handling/registries/common";
 
 export function assertIsObject(v: unknown): asserts v is Record<string, unknown> {
   if (v === null || (typeof v !== 'object' && typeof v !== 'function')) {
     // function is allowed because functions have properties too
-    throw new TypeError('Expected object-like value');
+    throw new ProgrammerError({
+      errorObject: ProgrammerErrorRegistry.byCode.BUG
+    });
   }
 }
